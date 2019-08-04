25e721b3845db39d744f5af4843a5ce028902914
Riley Chetwood <riley.chetwood@sjsu.edu>
2019-08-03 18:15:39 -0700

    cleared main for easier merge

--- src/main/java/edu/sjsu/cs157a/App.java
+++ src/main/java/edu/sjsu/cs157a/App.java
@@ -7,18 +7,6 @@ import edu.sjsu.cs157a.models.User;
 public class App {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
-		
-		LoginController lc = new LoginController();
-		SearchController sc = new SearchController();
-		User currentUser = null;
-		
-		
-		currentUser = lc.login();
-		
-		System.out.print(currentUser);
-		
-		sc.run();
-		
+
 	}
 }
